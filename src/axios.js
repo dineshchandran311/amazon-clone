100d8f4722f6c694c610a85ec05842a026e9ee3a
Dinesh Chandran <dineshchandran311@gmail.com>
2021-12-12 10:55:49 +0530

    payment

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://git.heroku.com/amazon-clone-cd-311.git' // THE API (cloud function) URL
+    baseURL: 'http://localhost:5001/clone-cd311/us-central1/api' // THE API (cloud function) URL
 });
 
 export default instance;
